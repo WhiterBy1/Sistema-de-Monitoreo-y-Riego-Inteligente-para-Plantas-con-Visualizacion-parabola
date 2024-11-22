27df83db7a2ea8d3ecba77ca3a1a46d13f7b31c9
JoseCPJ <j.quinonez0810@gmail.com>
2024-11-22 17:37:38 -0500

    Last change before project day

--- Esp32_collect_send_data/Esp32_collect_send_data.ino
+++ Esp32_collect_send_data/Esp32_collect_send_data.ino
@@ -67,8 +67,8 @@ void loop() {
   float humedadDHT11 = leerHumedadDHT(dht11, "DHT11");
 
   // Generar valores LM35 aproximados a los de los DHT con una fluctuación aleatoria
-  float tempLM35_1 = tempDHT11 + random(-5, 3);
-  float tempLM35_2 = tempDHT11 + random(-3, 2);
+  float tempLM35_1 = leerLm35(LM35_PIN1);
+  float tempLM35_2 = leerLm35(LM35_PIN1);
 
   // Leer humedad de los sensores de suelo y calcular promedio
   float humedadSuelo1 = leerHumedadSuelo(SOIL_PIN1);
